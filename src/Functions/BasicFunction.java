a595ffb569c6a0ef252801de7470ee30b78908dd
Aditya Kumar <aditya.cse24@gmail.com>
2025-08-30 17:40:56 +0530

    Initial commit - Java programs

---
+++ src/Functions/BasicFunction.java
@@ -0,0 +1,12 @@
+package Functions;
+
+public class BasicFunction {
+    public static void main(String[] args) {
+        Greet();
+    }
+    static void Greet() {
+        System.out.println("Hello World");
+    }
+}
+
+
