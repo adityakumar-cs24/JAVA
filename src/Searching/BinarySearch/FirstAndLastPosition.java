8367b43e6152cd45798afa9765354e5658468e05
Aditya Kumar <aditya.cse24@gmail.com>
2025-09-08 22:36:47 +0530

    Initial Commit

---
+++ src/Searching/BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,50 @@
+package Searching.BinarySearch;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end) {
+        //find the middle element
+        //int mid = (start + end) / 2; this may exceed the int range
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if (target > nums[mid]) {
+                start = mid + 1;
+            }
+            else {
+                // potential answer may be found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
